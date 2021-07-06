fc728c71ccd87bd7deb3b0d7e6ffb1ab00fe65b2
orianM <86421120+orianM@users.noreply.github.com>
2021-07-06 21:35:51 +0300

    Add files via upload

---
+++ OOP-HW/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region question 1
+
+            /*Object Oriented Programming or OOP for short, is: A way of programming - that helps develop code and
+              solve problems from a realistic approach (Meaning: how we would handle a problem if we were to meet
+              it in our daily lives). Instead of looking at our code as some meaningless ability we want to execute.
+              
+              OOP works using categories(parent) & subcategories(child) in our code -  by using "objects & classes"
+              and giving them "fields & properties". 
+              you can modify/change any object and give it specific properties.
+            
+              for example: if you were to make program that gives information about different types of vehicles -
+              trains, cars and bicycles their parent category would be "Vehicles" and will include a "field" which
+              all those vehicles have like wheels and then create a subcategory for each since they all have different
+              sized/shaped wheels.
+
+              That way you can add more and more types of vehicles (planes, tractors, trucks),
+              you would create a subcategory for each one, and the program will know that the main category is:
+              vehicles.*/
+
+            #endregion
+
+            #region question 2
+
+            /*C - is not a oop programming language
+              C++ - is partially oop but not fully*/
+
+            #endregion
+
+            #region question 3
+
+            /*Inheritance - is when a class inherits fields from another class.
+              For example: if you were to create a class called "Pens" and give it a field called "Color"
+              you would also be able Inherit the color field from class "Pens" and give each pen different colors.
+              
+              class name: Pens
+              field: color
+              Red Pen
+              Green Pen
+              Blue Pen*/
+
+            #endregion
+
+            #region question 4
+
+            /*Encapsulation and Abstraction examples:
+              1.Metal detector - checks if someone is carrying metal and gives a signal if it detects metal.
+              2.Train ticket machine - waits till customer pays and prints a train ticket to chosen destination*/
+
+            #endregion
+
+            #region question 5
+
+            /*Inheritance examples:
+              1.Musical Instruments - produce sounds, but each instrument has different sounds.
+              2.Clothes - all clothes have a size, but each piece of clothing has a different size.
+              3.Books - all books have pages, but each books has a different amount of pages.*/
+
+            #endregion
+
+            #region question 6
+
+            /*A car doesn't inherit from a bus because buses are much bigger than cars and also have a larger capacity
+              than cars.*/
+
+            #endregion
+
+            #region question 7
+
+            /*PolyMorphism example: shoes, can have different colors, shapes, sizes, materials.*/
+
+            #endregion
+
+            #region question 8
+
+            /*Object list examples:
+              1.Electronics - Air conditioner, Printer, Phone.
+              2.Furniture - Couch, Chair, Desk*/
+
+            #endregion
+        }
+    }
+}
